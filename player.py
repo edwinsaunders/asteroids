041563bec570f4a227ab7ef2d0d4c296f325c80b
edwinsaunders <edwinsaunders36@gmail.com>
2024-10-16 23:48:18 -0400

    9: 3.4 done - bullets now shoot every 0.3 of a second

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.timer = 0
 
 
 	def triangle(self):
@@ -21,6 +22,9 @@ class Player(CircleShape):
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "0xFFFFFF", self.triangle(), width=2)
 
+		# draw hit circle for diagnostic use
+		# pygame.draw.circle(screen, "0xFFFFFF", self.position, self.radius, width=2)
+
 	def rotate(self, dt):
 		self.rotation += PLAYER_TURN_SPEED * dt
 
@@ -28,9 +32,13 @@ class Player(CircleShape):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
 		self.position += forward * PLAYER_SPEED * dt
 
-	def shoot(self):
+	def shoot(self):		
+		if self.timer > 0:
+			return
 		shot = Shot(self.position.x, self.position.y)
 		shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+		self.timer = PLAYER_SHOOT_COOLDOWN
+		
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
@@ -45,3 +53,5 @@ class Player(CircleShape):
 			self.rotate(dt)
 		if keys[pygame.K_SPACE]:
 			self.shoot()
+
+		self.timer -= dt
